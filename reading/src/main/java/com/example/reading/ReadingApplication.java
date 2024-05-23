3b1992d92b6efd7c1fbc74f3a260be36d52ae14a
daiki_windows <dddaiki3150@gmail.com>
2024-05-23 12:52:28 +0900

    タイムゾーンの変更

--- reading/src/main/java/com/example/reading/ReadingApplication.java
+++ reading/src/main/java/com/example/reading/ReadingApplication.java
@@ -1,7 +1,11 @@
 package com.example.reading;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ReadingApplication {
@@ -9,5 +13,11 @@ public class ReadingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReadingApplication.class, args);
 	}
+	
+	@PostConstruct
+    public void init() {
+        // アプリケーション全体でのデフォルトタイムゾーンを設定
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tokyo"));
+    }
 
 }
